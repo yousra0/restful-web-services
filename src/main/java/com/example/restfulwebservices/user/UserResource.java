580b08155b6a99fe21ae908f9b072b8674635360
yousra_chaieb <100976916+yousra0@users.noreply.github.com>
2024-07-10 13:02:39 +0100

    commit

--- src/main/java/com/example/restfulwebservices/user/UserResource.java
+++ src/main/java/com/example/restfulwebservices/user/UserResource.java
@@ -2,9 +2,14 @@ package com.example.restfulwebservices.user;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class UserResource
 {
@@ -20,5 +25,11 @@ public class UserResource
     public User retriveUser(@PathVariable int id)
     {
         return service.findOne(id);
+    }
+    //method that posts a new user detail
+    @PostMapping("/users")
+    public void createUser(@RequestBody User user)
+    {
+        User sevedUser=service.save(user);
     }
 }
