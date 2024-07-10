99b3919dff0c8eb5f6138b57e0cacb9b54231e81
yousra_chaieb <100976916+yousra0@users.noreply.github.com>
2024-07-10 13:28:20 +0100

    commit

--- src/main/java/com/example/restfulwebservices/exception/ExceptionResponse.java
+++ src/main/java/com/example/restfulwebservices/exception/ExceptionResponse.java
@@ -1,2 +1,8 @@
-package com.example.restfulwebservices.exception;public class ExceptionResponse {
+package com.example.restfulwebservices.exception;
+
+import java.util.Date;
+
+public class ExceptionResponse {
+    public ExceptionResponse(Date date, String message, String description) {
+    }
 }
--- src/main/java/com/example/restfulwebservices/user/UserNotFoundException.java
+++ src/main/java/com/example/restfulwebservices/user/UserNotFoundException.java
@@ -1,2 +1,12 @@
-package com.example.restfulwebservices.user;public class UserNotFoundException {
+package com.example.restfulwebservices.user;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class UserNotFoundException extends RuntimeException
+{
+    public UserNotFoundException(String message)
+    {
+        super(message);
+    }
 }
