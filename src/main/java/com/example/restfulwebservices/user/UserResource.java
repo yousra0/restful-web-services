adca82bfbbf71d438515c5e587b0ab02d219c74d
yousra_chaieb <100976916+yousra0@users.noreply.github.com>
2024-07-10 13:28:20 +0100

    commit

--- src/main/java/com/example/restfulwebservices/user/UserResource.java
+++ src/main/java/com/example/restfulwebservices/user/UserResource.java
@@ -1,35 +1,42 @@
 package com.example.restfulwebservices.user;
-
+import java.net.URI;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-import java.util.List;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
-public class UserResource
-{
+public class UserResource {
+
     @Autowired
     private UserDaoService service;
+
     @GetMapping("/users")
-    public List<User> retriveAllUsers()
-    {
+    public List<User> retrieveAllUsers() {
         return service.findAll();
     }
-    //retrieves a specific user detail
+
     @GetMapping("/users/{id}")
-    public User retriveUser(@PathVariable int id)
-    {
-        return service.findOne(id);
+    public User retrieveUser(@PathVariable int id) {
+        User user = service.findOne(id);
+        if (user == null) {
+            throw new UserNotFoundException("id: " + id);
+        }
+        return user;
     }
-    //method that posts a new user detail
+
     @PostMapping("/users")
-    public void createUser(@RequestBody User user)
-    {
-        User sevedUser=service.save(user);
+    public ResponseEntity<Object> createUser(@RequestBody User user) {
+        User savedUser = service.save(user);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+        return ResponseEntity.created(location).build();
     }
 }
